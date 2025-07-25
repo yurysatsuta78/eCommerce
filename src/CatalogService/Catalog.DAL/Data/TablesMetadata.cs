f978910001aae16a1ded8f16ec42b790be7fbf17
Yury Sacuta <sacuta.u78@gmail.com>
2025-07-25 16:16:26 +0300

    Fix table aliases.

--- src/CatalogService/Catalog.DAL/Data/TablesMetadata.cs
+++ src/CatalogService/Catalog.DAL/Data/TablesMetadata.cs
@@ -5,19 +5,19 @@
         public static class CatalogItem
         {
             public const string Name = "catalog_item";
-            public const string Alias = "сi";
+            public const string Alias = "ci";
         }
 
         public static class CatalogCategory
         {
             public const string Name = "catalog_category";
-            public const string Alias = "сс";
+            public const string Alias = "cc";
         }
 
         public static class CatalogBrand
         {
             public const string Name = "catalog_brand";
-            public const string Alias = "сb";
+            public const string Alias = "cb";
         }
     }
 }
