39fd290908b9254392e2be43e2b0c7d717dabf9f
Yury Sacuta <sacuta.u78@gmail.com>
2025-07-25 16:27:12 +0300

    Fix entity dtos.

--- src/CatalogService/Catalog.BLL/Dto/Response/CatalogItem/CatalogItemDto.cs
+++ src/CatalogService/Catalog.BLL/Dto/Response/CatalogItem/CatalogItemDto.cs
@@ -2,5 +2,12 @@
 
 namespace Catalog.BLL.Dto.Response.CatalogItem
 {
-    public record CatalogItemDto(Guid ItemId, string Name, string Description, decimal Price, int AvailableStock) : IEntityDto;
+    public record CatalogItemDto : IEntityDto 
+    {
+        public Guid ItemId { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public decimal Price { get; init; }
+        public int AvailableStock { get; init; }
+    }
 }
