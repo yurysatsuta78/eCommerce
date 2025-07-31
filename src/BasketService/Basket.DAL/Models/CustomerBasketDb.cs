1681a47f8c24e6c867a9b76293f5fed1a78c6c2d
Yury Sacuta <sacuta.u78@gmail.com>
2025-07-31 14:18:36 +0300

    Add database models.

---
+++ src/BasketService/Basket.DAL/Models/CustomerBasketDb.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.DAL.Models
+{
+    public class CustomerBasketDb
+    {
+        public Guid CustomerId { get; set; }
+        public List<BasketItemDb> Items { get; set; } = new();
+    }
+}
