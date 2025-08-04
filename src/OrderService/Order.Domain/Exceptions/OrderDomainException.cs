faeda3613cd0a0a33f758c698c2a70233417e6fe
Yury Sacuta <sacuta.u78@gmail.com>
2025-08-04 16:43:54 +0300

    Add domain models.

---
+++ src/OrderService/Order.Domain/Exceptions/OrderDomainException.cs
@@ -0,0 +1,11 @@
+﻿namespace Order.Domain.Exceptions
+{
+    public class OrderDomainException : Exception
+    {
+        public OrderDomainException() { }
+        public OrderDomainException(string message) 
+            : base(message) { }
+        public OrderDomainException(string message, Exception innerException) 
+            : base(message, innerException) { }
+    }
+}
